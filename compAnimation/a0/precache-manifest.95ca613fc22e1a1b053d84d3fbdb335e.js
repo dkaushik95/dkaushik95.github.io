caf887564ff90f208935e4d097897334c9105591
Dishant Kaushik <dkaushik95@gmail.com>
2019-09-05 09:55:06 -0400

    Added a section for assignments

---
+++ compAnimation/a0/precache-manifest.95ca613fc22e1a1b053d84d3fbdb335e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3962f958a7c3f47d3e48eccc40e3a2b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f46e4333351da51e08b0",
+    "url": "/static/css/main.54f8e0e3.chunk.css"
+  },
+  {
+    "revision": "3ff7cc8da0bfa253bb5c",
+    "url": "/static/js/2.d6d572b2.chunk.js"
+  },
+  {
+    "revision": "f46e4333351da51e08b0",
+    "url": "/static/js/main.50d8bb14.chunk.js"
+  },
+  {
+    "revision": "d3ccc54a46fc6f435ab1",
+    "url": "/static/js/runtime~main.159cf062.js"
+  }
+]);
